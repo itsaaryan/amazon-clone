2836477e2dd283462ffd6bd63aaca41eb873150f
Aaryan <aaryan359500@gmail.com>
2020-10-26 16:14:33 +0530

    updated payment section

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,46 @@
 import React from "react";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
+import "./Payment.css";
 
 function Payment() {
-  return <div>Payment</div>;
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className="payment">
+      <div className="payment-container">
+        <div className="payment-section">
+          <div className="payment-title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment-address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angles,CA</p>
+          </div>
+        </div>
+        <div className="payment-section">
+          <div className="payment-title">
+            <h3>
+              Review Items <strong>&</strong> delivery
+            </h3>
+          </div>
+          <div className="payment-items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment-section"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Payment;
